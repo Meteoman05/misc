258efa1cf6e2f9c609c4fbca497bc8e82e2e3daf
Meteoman05 <76489240+Meteoman05@users.noreply.github.com>
2022-01-13 23:24:10 +0300

    Update Рекурсия.py

--- Рекурсия.py
+++ Рекурсия.py
@@ -9,7 +9,6 @@ def from_10(num, to_base, alph=string.printable):
     else:
         return from_10(num // to_base, to_base) + alph[num % to_base]
 
-dict(enumerate(string.printable))
 
 def to_10(num, from_base, alph=string.printable, i=0):
     alph_ = {j[1]:j[0] for j in enumerate(alph)}
