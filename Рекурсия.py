d7f2b18c26d242efce369d7779c5796e7b71d5bf
Meteoman05 <76489240+Meteoman05@users.noreply.github.com>
2022-01-14 09:55:33 +0300

    Update Рекурсия.py

--- Рекурсия.py
+++ Рекурсия.py
@@ -1,5 +1,6 @@
 import string
 
+
 def from_10(num, to_base, alph=string.printable):
     if num // to_base < to_base:
         if num // to_base == 0:
@@ -15,6 +16,14 @@ def to_10(num, from_base, alph=string.printable, i=0):
     if i == len(num)-1:
         return int(alph_[num[i]])
     else:
-        return int(alph_[num[i]])*from_base**(len(num)-1-i) + to_10(num, from_base,
-                                                          alph=string.printable,
-                                                          i = i + 1)
+        return (int(alph_[num[i]])*from_base**(len(num)-1-i) +
+    to_10(num,
+          from_base,
+          alph=string.printable,
+          i = i + 1))
+
+
+def num_to_min(num, alph=string.printable):
+        alph_ = {j[1]:j[0] for j in enumerate(alph)}
+        max_ = max(alph_[i] for i in num)
+        return from_10(to_10(num, max_+1, alph=alph), 100)
