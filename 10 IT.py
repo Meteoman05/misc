3f352815eb3eca4f10d2c8751615a3350c1b313c
Meteoman05 <76489240+Meteoman05@users.noreply.github.com>
2022-02-16 12:12:52 +0300

    Update 10 IT.py

--- 10 IT.py
+++ 10 IT.py
@@ -1,4 +1,5 @@
 import random
+import pprint
 
 left = [i for i in input('Введите отсутствующих людей\n').split()]
 
@@ -9,8 +10,17 @@ IT = [['Дереник', 'Паша З', 'Гриша', 'Вова', 'Дамир'],
       ['Федя', 'Ярик', None, 'Ева', 'Полина'],
       ['Саша Коч', 'Илья', 'Глеб', 'Паша А', 'Венера']]
 
+
+for i in range(len(IT)):
+      for j in range(len(IT[i])):
+            for g in left:
+                  if g == IT[i][j]:
+                        IT[i][j] = None
+                  
+
+
 #ch = int(input('Введите номер ряда >>>'))
 #v = int(input('Введите номер варианта >>>'))
 
-print(IT)
+pprint.pprint(IT)
       
